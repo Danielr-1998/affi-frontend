4145e089f95ce0cc2d139cfb2d0dd17a039fe168
Danielr-1998 <daniel.riano@utp.edu.co>
2025-05-09 12:47:52 -0500

    Config

--- apps/org/eslint.config.mjs
+++ apps/org/eslint.config.mjs
@@ -1,8 +1,26 @@
 import nx from '@nx/eslint-plugin';
-import baseConfig from '../../eslint.config.mjs';
 
 export default [
-  ...baseConfig,
+  ...nx.configs['flat/base'],
+  ...nx.configs['flat/typescript'],
+  ...nx.configs['flat/javascript'],
+  {
+    ignores: ['**/dist'],
+  },
+  {
+    files: [
+      '**/*.ts',
+      '**/*.tsx',
+      '**/*.cts',
+      '**/*.mts',
+      '**/*.js',
+      '**/*.jsx',
+      '**/*.cjs',
+      '**/*.mjs',
+    ],
+    // Override or add rules here
+    rules: {},
+  },
   ...nx.configs['flat/angular'],
   ...nx.configs['flat/angular-template'],
   {
